93a01b05095817103d77d14fdd5c38a14701ad4d
Austin LaShure <austinglashure@gmail.com>
2022-05-27 20:15:30 -0400

    added a randomizer for where the bombs are placed

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import random
+import math
+
 print("Welcome to Minesweeper!\n")
 width = int(input("How wide do you want the board? (max is 10)\n"))
 length = int(input ("How tall do you want the board? (max is 10)\n"))
@@ -19,10 +22,23 @@ class Tile:
 
 
 board = []
+counter = 0
+amount = round(math.sqrt(width*length))
+print(amount)
+size = width*length
+print(size)
+bomb_positions = [random.randint(0, size-1) for i in range(amount)]
+print(bomb_positions)
 for i in range(width):
     for j in range(length):
-        tile = Tile([i, j], True, board)
-        board.append(tile)
+        if counter in bomb_positions:
+            tile = Tile([i, j], True, board)
+            board.append(tile)
+        else:
+            tile = Tile([i, j], False, board)
+            board.append(tile)
+        counter += 1
 
 for tile in board:
+    print(f"pos: {tile.x}{tile.y}")
     print(tile.isBomb)
