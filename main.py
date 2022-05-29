8c254e3b3a5e869b376ebf211c7a3cd0fa4a5f1c
Austin <austinglashure@gmail.com>
2022-05-29 19:30:48 -0400

    can create blank board and display it

--- main.py
+++ main.py
@@ -1,44 +1,18 @@
-import random
-import math
+import numpy as np
 
-print("Welcome to Minesweeper!\n")
-width = int(input("How wide do you want the board? (max is 10)\n"))
-length = int(input ("How tall do you want the board? (max is 10)\n"))
-print(f"Okay! Creating the board with dimensions {length}x{width}\n")
-bomb = '@'
-tile = '#'
-empty = 'O'
+def display_board(board):
+    print(board)
 
-class Tile:
-    def __init__(self, position, hasBomb, board):
-        self.x = position[0]
-        self.y = position[1]
-        self.isBomb = hasBomb
-        self.initial_value = tile
-        if self.isBomb:
-            self.under_value = bomb
-        else:
-            self.under_value = empty
+def create_board(x, y, num_bombs):
+    print(f"You want {num_bombs} bombs\n")
+    return np.zeros((x, y))
 
+print("Welcome to Minesweeper!")
 
-board = []
-counter = 0
-amount = round(math.sqrt(width*length))
-print(amount)
-size = width*length
-print(size)
-bomb_positions = [random.randint(0, size-1) for i in range(amount)]
-print(bomb_positions)
-for i in range(width):
-    for j in range(length):
-        if counter in bomb_positions:
-            tile = Tile([i, j], True, board)
-            board.append(tile)
-        else:
-            tile = Tile([i, j], False, board)
-            board.append(tile)
-        counter += 1
+x = int(input("How wide do you want the game board?\n"))
+y = int(input("How tall do you want it?\n"))
+bombs = int(input("How many bombs do you want?\n"))
 
-for tile in board:
-    print(f"pos: {tile.x}{tile.y}")
-    print(tile.isBomb)
+game_board = create_board(x, y, bombs)
+
+display_board(game_board)
